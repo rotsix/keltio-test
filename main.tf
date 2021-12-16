1ea4c3891e1cf35c1ed4c02cd2aa4e82a126f4d8
Victor Franzi <victor.franzi@gmail.com>
2021-12-16 17:57:19 +0100

    wip: add a subnet for each service

--- main.tf
+++ main.tf
@@ -58,12 +58,24 @@ resource "aws_security_group" "allow_db_con" {
     to_port         = 3306
     protocol        = "tcp"
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = local.availability_zone
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "db_subnet"
+  subnet_ids = [aws_subnet.subnet.id]
 }
 
 
@@ -91,8 +103,8 @@ resource "aws_instance" "app" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   ebs_optimized          = true
-  subnet_id              = aws_subnet.subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id              = aws_subnet.subnet.id
 }
 
 # create app storage
@@ -118,6 +130,7 @@ resource "aws_rds_cluster" "db_cluster" {
   master_password        = var.db_password
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.allow_db_con.id]
+  db_subnet_group_name   = aws_db_subnet_group.default.name
 }
 
 # create db instance
